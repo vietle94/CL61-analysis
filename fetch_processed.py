5c4340531c32e5afb4cf0e64fa5378fdffeefa1a
Le Viet (FMI) <viet.le@fmi.fi>
2024-02-14 16:30:03 +0200

    second commit

--- fetch_processed.py
+++ fetch_processed.py
@@ -3,18 +3,20 @@ import os
 
 url = 'https://cloudnet.fmi.fi/api/raw-files'
 params = {
-    'dateFrom': '2023-01-01',
-    'dateTo': '2023-09-01',
+    'dateFrom': '2023-08-01',
+    'dateTo': '2024-02-01',
     'site': 'kenttarova',
     'instrument': 'cl61d'
 }
 metadata = requests.get(url, params).json()
 
 for row in metadata:
-    file_path = 'D:/CloudnetData/Kenttarova/CL61/Raw/' + row['filename']
+    file_path = 'D:/CloudnetData/Kenttarova/CL61/Processed/' + row['filename']
     if os.path.isfile(file_path):
         continue
     if 'live' in row['s3key']:
+        continue
+    else:
         print(row['filename'])
         res = requests.get(row['downloadUrl'])
         with open(file_path, 'wb') as f:
