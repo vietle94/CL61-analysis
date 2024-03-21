4c3b1b2d5dc22555f29f16e69fa9bcd945ca6b55
Le Viet (FMI) <viet.le@fmi.fi>
2024-03-21 15:33:14 +0200

    separate calibration script for each site

--- plot_calibration_rovaniemi.py
+++ plot_calibration_rovaniemi.py
@@ -10,27 +10,19 @@ import xarray as xr
 myFmt = mdates.DateFormatter('%H:%M')
 
 # %%
-file_path = glob.glob("G:\CloudnetData\Kenttarova\CL61\Raw/live_2024030[5-6]*.nc")
-file_save = "G:\CloudnetData\Kenttarova\CL61\Calibration/"
+file_path = glob.glob("G:/rovaniemi/20240311/*.nc") #'sw_version': '1.2.7',
+file_save = "G:/rovaniemi\Calibration/"
 
 # %%
 df = xr.open_mfdataset(file_path)
 df = df.isel(range=slice(1, None))
 
 # %%
-df_ = df.sel(time=slice(pd.to_datetime('20240305 000000'),
-                        pd.to_datetime('20240307 000000')))
-
+df_ = df.sel(time=slice(pd.to_datetime('20240311 090000'),
+                        pd.to_datetime('20240311 170000')))
+# df_ = df
 ppol = df_.p_pol/(df_.range**2)
 xpol = df_.x_pol/(df_.range**2)
-
-# %%
-# fig, ax = plt.subplots()
-# p = ax.pcolormesh(df.time, df.range,
-#               ppol.T, shading='nearest', cmap='RdBu',
-#               norm=SymLogNorm(linthresh=1e-17, vmin=-2e-12, vmax=2e-12))
-# ax.xaxis.set_major_formatter(myFmt)
-# fig.colorbar(p, ax=ax)
 
 # %%
 for hmin, hmax in ((1000, 3000), (3000, 5000), (5000, 7000),
@@ -66,20 +58,20 @@ for hmin, hmax in ((1000, 3000), (3000, 5000), (5000, 7000),
     ax[1, 1].set_ylim([4.5e-15, 7.5e-15])
     ax[1, 1].set_ylabel(r"$\sigma_{xpol}$")
     for ax_ in ax.flatten():
-        ax_.axvspan(pd.to_datetime('20240305 131100'), pd.to_datetime('20240306 104200'), facecolor='gray',
+        ax_.axvspan(pd.to_datetime('20240311 095500'), pd.to_datetime('20240311 141000'), facecolor='gray',
                         alpha=0.3)
         ax_.xaxis.set_major_formatter(myFmt)
         ax_.grid()
     fig.suptitle(f"From {hmin} m to {hmax} m")
     fig.savefig(file_save + f'ppol_xpol_{hmin}_{hmax}.png', dpi=600)
 
-# %%
-ppol_mean_time = ppol.sel(time=slice(pd.to_datetime('20240305 131500'), pd.to_datetime('20240306 103800'))).mean(dim='time')
-ppol_std_time = ppol.sel(time=slice(pd.to_datetime('20240305 131500'), pd.to_datetime('20240306 103800'))).std(dim='time')
-xpol_mean_time = xpol.sel(time=slice(pd.to_datetime('20240305 131500'), pd.to_datetime('20240306 103800'))).mean(dim='time')
-xpol_std_time = xpol.sel(time=slice(pd.to_datetime('20240305 131500'), pd.to_datetime('20240306 103800'))).std(dim='time')
+# %% rovaniemi
+time0, time1 = (pd.to_datetime('20240311 115500'), pd.to_datetime('20240311 141000'))
+ppol_mean_time = ppol.sel(time=slice(time0, time1)).mean(dim='time')
+ppol_std_time = ppol.sel(time=slice(time0, time1)).std(dim='time')
+xpol_mean_time = xpol.sel(time=slice(time0, time1)).mean(dim='time')
+xpol_std_time = xpol.sel(time=slice(time0, time1)).std(dim='time')
 
-# %%
 fig, ax = plt.subplots(2, 2, sharey=True, constrained_layout=True,
                        figsize=(9, 6))
 ax[0, 0].plot(ppol_mean_time, ppol_mean_time.range, '.')
@@ -87,7 +79,7 @@ ax[0, 0].set_xlim([-1e-13, 1e-13])
 ax[0, 0].set_xlabel(r"$\mu_{ppol}$")
 
 ax[1, 0].plot(ppol_std_time, ppol_std_time.range, '.')
-ax[1, 0].set_xlim([2.5e-14, 4.5e-14])
+ax[1, 0].set_xlim([4e-15, 1e-14])
 ax[1, 0].set_xlabel(r"$\sigma_{ppol}$")
 
 
@@ -96,21 +88,36 @@ ax[0, 1].set_xlim([-1e-13, 1e-13])
 ax[0, 1].set_xlabel(r"$\mu_{xpol}$")
 
 ax[1, 1].plot(xpol_std_time, xpol_std_time.range, '.')
-ax[1, 1].set_xlim([2.5e-14, 4.5e-14])
+ax[1, 1].set_xlim([4e-15, 1e-14])
 ax[1, 1].set_xlabel(r"$\sigma_{xpol}$")
 
 for ax_ in ax.flatten():
     ax_.grid()
 fig.savefig(file_save + 'ppol_xpol_profile.png', dpi=600)
-
 ax[0, 0].set_ylim([0, 3000])
+ax[0, 0].set_xlim([-0.25e-13, 0.75e-13])
+ax[0, 1].set_xlim([-0.25e-13, 0.75e-13])
+ax[1, 0].set_xlim([4e-15, 8e-15])
+ax[1, 1].set_xlim([4e-15, 8e-15])
 fig.savefig(file_save + 'ppol_xpol_profile_near.png', dpi=600)
 
-ax[0, 0].set_xlim([-1e-14, 1e-14])
-ax[0, 1].set_xlim([-1e-14, 1e-14])
-ax[1, 0].set_xlim([3.25e-14, 3.75e-14])
-ax[1, 1].set_xlim([3.25e-14, 3.75e-14])
+ax[0, 0].set_xlim([-2e-15, 6e-15])
+ax[0, 1].set_xlim([-2e-15, 6e-15])
 fig.savefig(file_save + 'ppol_xpol_profile_nearnear.png', dpi=600)
+
+# %%
+fig, ax = plt.subplots(1, 2, figsize=(9, 4), sharey=True,
+                       constrained_layout=True)
+p = ax[1].pcolormesh(df['time'], df['range'], df['overlap_function'].T)
+fig.colorbar(p, ax=ax[1])
+ax[1].set_ylim([0, 500])
+ax[1].xaxis.set_major_locator(mdates.HourLocator(interval=4))
+ax[1].xaxis.set_major_formatter(myFmt)
+
+ax[0].plot(df['overlap_function'].isel(time=0).values, df.range, '.')
+ax[0].set_ylim([0, 500])
+ax[0].grid()
+fig.savefig(file_save + 'overlap_function.png', dpi=600)
 
 # %%
 df_monitoring = xr.open_mfdataset(file_path, group='monitoring')
@@ -125,6 +132,6 @@ for val, ax_ in zip(['background_radiance', 'internal_temperature',
     ax_.set_ylabel(val)
     ax_.xaxis.set_major_formatter(myFmt)
     ax_.grid()
-    ax_.axvspan(pd.to_datetime('20240305 131100'), pd.to_datetime('20240306 104200'), facecolor='gray',
+    ax_.axvspan(pd.to_datetime('20240311 095500'), pd.to_datetime('20240311 141000'), facecolor='gray',
                     alpha=0.3)
 fig.savefig(file_save + 'monitoring.png', dpi=600)
