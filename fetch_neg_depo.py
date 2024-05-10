b54f95fe9d3f9fae6cc252f9216bc873a26a7ced
Le Viet (FMI) <viet.le@fmi.fi>
2024-05-10 18:30:40 +0300

    add plot_calibration function

---
+++ fetch_neg_depo.py
@@ -0,0 +1,50 @@
+import numpy as np
+import pandas as pd
+import xarray as xr
+import netCDF4
+import glob
+import os
+import func
+import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
+
+# %%
+depo_save = r'G:\CloudnetData\Kenttarova\CL61\Files_analysis\Negative_depo/'
+files = glob.glob('G:\CloudnetData\Kenttarova\CL61/Raw_processed/' + '*.nc')
+
+# %%
+for file in files[:[i for i, x in enumerate(files) if '20230621' in x][0]]:
+    print(file)
+    file_name = file.split('.')[0].split('\\')[-1]
+    check_file = depo_save + file_name + '_neg_depo.csv'
+    if os.path.isfile(check_file):
+        print('yes')
+        continue
+    try:
+        df = xr.open_dataset(file)
+    except OSError:
+        print('Bad file')
+        continue
+
+    df_range = np.tile(df['range'], df['time'].size).reshape(df['time'].size, -1)
+    df['range_full'] = (['time', 'range'], df_range)
+    df_range = df['range_full'].where(~df['noise']).where(df['x_pol'] < 0).values
+    xpol = df['x_pol'].where(~df['noise']).where(df['x_pol'] < 0)
+    ppol = df['p_pol'].where(~df['noise']).where(df['x_pol'] < 0)
+    
+    mask = ~np.isnan(xpol)
+    xpol = xpol.values[mask]
+    ppol = ppol.values[mask]
+    df_range = df_range[mask]
+    if xpol.size < 1:
+        continue
+    
+    result = pd.DataFrame({
+        'datetime': df.time[0].values,
+        'xpol': xpol,
+        'ppol': ppol,
+        'range': df_range
+    })
+    file_name = file.split('.')[0].split('\\')[-1]
+    result.to_csv(check_file, index=False)
+    
--- func.py
+++ func.py
@@ -5,6 +5,7 @@ import xarray as xr
 from matplotlib.colors import LogNorm, SymLogNorm
 import matplotlib.dates as mdates
 import pywt
+myFmt = mdates.DateFormatter('%H:%M')
 
 def noise_detection(df, wavelet = 'bior1.1'):
     
@@ -28,3 +29,78 @@ def noise_detection(df, wavelet = 'bior1.1'):
     df['p_pol_smooth'] = (['range'], filtered)
     df.attrs['minimax_thresh'] = minimax_thresh
     return df
+
+def plot_calibration(df_, save_path,
+                     time_mean_lim=[-1e-13, 1e-13],
+                     time_std_lim=[2.5e-14, 4.5e-14],
+                     profile_mean_lim=[-1e-13, 1e-13],
+                     profile_std_lim=[3e-14, 2e-13]):
+    
+    # Calculate pure signals
+    ppol = df_.p_pol/(df_.range**2)
+    xpol = df_.x_pol/(df_.range**2)
+    
+    # Time series
+    for hmin, hmax in ((1000, 3000), (3000, 5000), (5000, 7000),
+                       (7000, 9000), (9000, 11000), (11000, 13000), (13000, 15000),
+                       (15000, 16000)):
+        ppol24 = ppol.sel(range=slice(hmin, hmax))
+        ppol_24 = ppol24.mean(dim='range')
+        ppol_24_std = ppol24.std(dim='range')
+        
+        xpol24 = xpol.sel(range=slice(hmin, hmax))
+        xpol_24 = xpol24.mean(dim='range')
+        xpol_24_std = xpol24.std(dim='range')
+        
+        fig, ax = plt.subplots(2, 2, sharex=True, constrained_layout=True,
+                               figsize=(12, 6))
+        ax[0, 0].plot(ppol_24.time, ppol_24.values, '.')
+        ax[0, 0].set_ylim(time_mean_lim)
+        ax[0, 0].set_ylabel(r"$\mu_{ppol}/range^2$")
+        
+        ax[1, 0].plot(ppol_24_std.time, ppol_24_std.values, '.')
+        ax[1, 0].set_ylim(time_std_lim)
+        ax[1, 0].set_ylabel(r"$\sigma_{ppol}/range^2$")
+        
+        ax[0, 1].plot(xpol_24.time, xpol_24.values, '.')
+        ax[0, 1].set_ylim(time_mean_lim)
+        ax[0, 1].set_ylabel(r"$\mu_{xpol}/range^2$")
+        
+        ax[1, 1].plot(xpol_24_std.time, xpol_24_std.values, '.')
+        ax[1, 1].set_ylim(time_std_lim)
+        ax[1, 1].set_ylabel(r"$\sigma_{xpol}/range^2$")
+        for ax_ in ax.flatten():
+            ax_.xaxis.set_major_formatter(myFmt)
+            ax_.grid()
+        fig.suptitle(f"From {hmin} m to {hmax} m")
+        fig.savefig(save_path + f'ppol_xpol_{hmin}_{hmax}.png', dpi=600)
+        plt.close(fig)
+        
+        # Profiles
+    fig, ax = plt.subplots(2, 2, sharey=True, constrained_layout=True,
+                           figsize=(9, 6))
+    ax[0, 0].plot(ppol.mean(dim='time'), ppol.range)
+    ax[0, 0].set_xlim(profile_mean_lim)
+    ax[0, 0].set_xlabel(r"$\mu_{ppol}/range^2$")
+    
+    ax[1, 0].plot(ppol.std(dim='time'), ppol.range)
+    ax[1, 0].set_xlim(profile_std_lim)
+    ax[1, 0].set_xlabel(r"$\sigma_{ppol}/range^2$")
+    
+    
+    ax[0, 1].plot(xpol.mean(dim='time'), xpol.range)
+    ax[0, 1].set_xlim(profile_mean_lim)
+    ax[0, 1].set_xlabel(r"$\mu_{xpol}/range^2$")
+    
+    ax[1, 1].plot(xpol.std(dim='time'), xpol.range)
+    ax[1, 1].set_xlim(profile_std_lim)
+    ax[1, 1].set_xlabel(r"$\sigma_{xpol}/range^2$")
+    
+    for ax_ in ax.flatten():
+        ax_.grid()
+    fig.savefig(save_path + 'ppol_xpol_profile.png', dpi=600)
+    
+    ax[0, 0].set_ylim([0, 1000])
+    fig.savefig(save_path + 'ppol_xpol_profile_near.png', dpi=600)
+    
+    plt.close(fig)
